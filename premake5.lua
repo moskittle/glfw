d36d1155c1b531b53cdfde05f6db8bb4f9579e4d
shifeng <sfmarc1iang@gmail.com>
2019-08-19 10:44:08 +0800

    Fix premake5.lua config issues

--- premake5.lua
+++ premake5.lua
@@ -7,7 +7,7 @@ project "GLFW"
 
     files
     {
-        "include/GLFW/glfw.3h",
+        "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
         "src/context.c",
@@ -19,13 +19,13 @@ project "GLFW"
     }
 
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
 
         files{
             "src/win32_init.c",
             "src/win32_joystick.c",
+            "src/win32_monitor.c",
             "src/win32_time.c",
             "src/win32_thread.c",
             "src/win32_window.c",
