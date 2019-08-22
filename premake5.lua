79414bf65f6193491638b17fa2a98da64f7f9df3
shifeng <sfmarc1iang@gmail.com>
2019-08-22 17:25:21 +0800

    Updated premake file

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-interm/" .. outputdir .. "/%{prj.name}")
@@ -20,7 +21,6 @@ project "GLFW"
 
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
         files{
             "src/win32_init.c",
@@ -39,6 +39,12 @@ project "GLFW"
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
         }
-    
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+    filter { "configurations:Debug" }
+        runtime "Debug"
+        symbols "on"
+	
+    filter { "configurations:Release" }
+        runtime "Release"
+        optimize "on"
+        
